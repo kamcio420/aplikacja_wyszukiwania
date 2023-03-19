d1c276ed6e9a1b2058638cdf933585c4a81b5f48
kamillachdev <kamillachdev@gmail.com>
2023-03-19 12:03:22 +0100

    brute force algorithm and string randomizer

--- app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
@@ -19,12 +19,42 @@ class MainActivity : AppCompatActivity() {
         val kmpOutput = findViewById<TextView>(R.id.kmpOutput)
         val bmOutput = findViewById<TextView>(R.id.bmOutput)
         val rkOutput = findViewById<TextView>(R.id.rkOutput)
+        var randomString = ""
+
+        //RANDOM LETTERS
+        fun randomLetters(amount: Int) {
+            randomString = ""
+            val str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPRSTUVWXYZ123456789"
+            for (i in 1..amount) {
+                randomString += str.random()
+            }
+        }
+
+        //BRUTE FORCE ALGORITHM
+        fun bruteForce(str: CharArray, substr: CharArray): Int {
+            if (substr.size > str.size) return 0
+
+            var occurrences = 0
+
+            for (cursor1 in 0 until str.size) {
+                var matchCount = 0
+                for (cursor2 in 0 until substr.size) {
+                    if (str[cursor1 + cursor2] == substr[cursor2]) matchCount++
+                }
+
+                if (matchCount == substr.size) occurrences++
+            }
+            return occurrences
+        }
 
         findViewById<Button>(R.id.uruchomButton).setOnClickListener {
 
             if(iloscInput.isNotEmpty() && wzorzecInput!="" )
             {
-                findViewById<TextView>(R.id.errorText).text = "git"
+                findViewById<TextView>(R.id.errorText).text = ""
+                randomLetters(parseInt(iloscInput))
+                val wynik = bruteForce(randomString.toCharArray(), wzorzecInput.toCharArray())
+                findViewById<TextView>(R.id.errorText).text = wynik.toString()
             }
             else
             {
