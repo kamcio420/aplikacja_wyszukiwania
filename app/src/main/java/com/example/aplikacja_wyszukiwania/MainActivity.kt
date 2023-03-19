e60da961b99463d83b0b2a3c0706d4184c6d2332
kamillachdev <kamillachdev@gmail.com>
2023-03-19 11:31:37 +0100

    button listener and correct input protection

--- app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.aplikacja_wyszukiwania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.uruchomButton).setOnClickListener {
+            val iloscInput = findViewById<TextInputEditText>(R.id.iloscZnakowInput)
+            val wzorzecInput = findViewById<TextInputEditText>(R.id.wzorzecInput)
+
+            if(parseInt(iloscInput.toString()) > 0 && wzorzecInput.length() >= 1)
+            {
+                val bruteForceOutput = findViewById<TextView>(R.id.bruteForceOutput)
+                val kmpOutput = findViewById<TextView>(R.id.kmpOutput)
+                val bmOutput = findViewById<TextView>(R.id.bmOutput)
+                val rkOutput = findViewById<TextView>(R.id.rkOutput)
+            }
+            else
+            {
+                findViewById<TextView>(R.id.errorText).text = "Wprowadź poprawne dane!"
+            }
+        }
     }
 }
