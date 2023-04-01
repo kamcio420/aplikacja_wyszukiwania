ca0f63f95694c035958f36dda36f3b24df6bf074
kamillachdev <kamillachdev@gmail.com>
2023-04-01 12:19:47 +0200

    little fixes, changed output format

--- app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
+++ app/src/main/java/com/example/aplikacja_wyszukiwania/MainActivity.kt
@@ -17,12 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         //DECLARATIONS
         val errorOutput = findViewById<TextView>(R.id.errorText)
-        val bfOccurrencesOutput = findViewById<TextView>(R.id.bfOccurrencesOutput)
-        val kpmOccurrencesOutput = findViewById<TextView>(R.id.kmpOccurrencesOutput)
-        val bruteForceOutput = findViewById<TextView>(R.id.bruteForceOutput)
-        val kmpOutput = findViewById<TextView>(R.id.kmpOutput)
-        val bmOutput = findViewById<TextView>(R.id.bmOutput)
-        val rkOutput = findViewById<TextView>(R.id.rkOutput)
+        val bfOutputTime = findViewById<TextView>(R.id.bruteForceOutput)
+        val kmpOutputTime = findViewById<TextView>(R.id.kmpOutput)
+        val bmOutputTime = findViewById<TextView>(R.id.bmOutput)
+        val rkOutputTime = findViewById<TextView>(R.id.rkOutput)
+        val occurrencesOutput = findViewById<TextView>(R.id.occurrencesText)
+        val firstPositionOutput = findViewById<TextView>(R.id.firstPositionText)
         var randomString = ""
 
         //RANDOM LETTERS
@@ -105,14 +105,115 @@ class MainActivity : AppCompatActivity() {
             return -1
         }
 
+        //BM ALGORITHM
+        fun bm(source: String, pattern: String): Int {
+
+            val pattChar = pattern.toCharArray()
+            val patternLength: Int = pattChar.size
+            if (patternLength == 0) {
+                return 0
+            }
+            val src = source.toCharArray()
+            val srcLength: Int = src.size
+
+            val shift = IntArray(200) { patternLength }
+            for (k in 0..(patternLength - 2)) {
+                shift[pattChar[k].toInt()] = patternLength - 1 - k
+            }
+
+            var i = 0
+            var j: Int
+            while ((i + patternLength) <= srcLength) {
+                j = patternLength - 1
+                while (source[i + j] == pattChar[j]) {
+                    j -= 1
+                    if (j < 0) {
+                        return i
+                    }
+                }
+                i += shift[src[i + patternLength - 1].toInt()]
+            }
+            return -1
+        }
+
+        //RK ALGORITHM
+
+            // d is the number of characters in the input alphabet
+            val d = 256
+            fun search(txt: String, pat: String, q: Int): Int {
+                val M = pat.length
+                val N = txt.length
+                var i: Int
+                var j: Int
+                var p = 0 // hash value for pattern
+                var t = 0 // hash value for txt
+                var h = 1
+
+                // The value of h would be "pow(d, M-1)%q"
+                i = 0
+                while (i < M - 1) {
+                    h = h * d % q
+                    i++
+                }
+
+                // Calculate the hash value of pattern and first
+                // window of text
+                i = 0
+                while (i < M) {
+                    p = (d * p + pat[i].code) % q
+                    t = (d * t + txt[i].code) % q
+                    i++
+                }
+
+                // Slide the pattern over text one by one
+                i = 0
+                while (i <= N - M) {
+
+
+                    // Check the hash values of current window of
+                    // text and pattern. If the hash values match
+                    // then only check for characters one by one
+                    if (p == t) {
+                        /* Check for characters one by one */
+                        j = 0
+                        while (j < M) {
+                            if (txt[i + j] != pat[j]) break
+                            j++
+                        }
+
+                        // if p == t and pat[0...M-1] = txt[i, i+1,
+                        // ...i+M-1]
+                        if (j == M)
+                            return i
+
+                    }
+
+                    // Calculate hash value for next window of text:
+                    // Remove leading digit, add trailing digit
+                    if (i < N - M) {
+                        t = ((d * (t - txt[i].code * h)
+                                + txt[i + M].code)
+                                % q)
+
+                        // We might get negative value of t,
+                        // converting it to positive
+                        if (t < 0) t = t + q
+                    }
+                    i++
+                }
+                return -1
+            }
+
 
         //URUCHOM BUTTON ACTION
         findViewById<Button>(R.id.uruchomButton).setOnClickListener {
 
             val iloscInput = findViewById<TextInputEditText>(R.id.iloscZnakowInput).text.toString()
             val wzorzecInput = findViewById<TextInputEditText>(R.id.wzorzecInput).text.toString()
-            val bfOutputOccurrences: Int
-            val kmpOutputOccurrences: Int
+            val bfOutput: Int
+            val kmpOutput: Int
+            val bmOutput: Int
+            val rkOutput: Int
 
             if(iloscInput.isNotEmpty() && wzorzecInput!="")
             {
@@ -123,18 +224,36 @@ class MainActivity : AppCompatActivity() {
 
                     //START BRUTE FORCE
                     val bruteForceTime = measureTimeMillis {
-                        bfOutputOccurrences = bruteForce(randomString.toCharArray(), wzorzecInput.toCharArray())
+                        bfOutput = bruteForce(randomString.toCharArray(), wzorzecInput.toCharArray())
                     }
-                    bruteForceOutput.text = bruteForceTime.toString()
-                    bfOccurrencesOutput.text = bfOutputOccurrences.toString()
+                    bfOutputTime.text = bruteForceTime.toString()
                     //END BRUTE FORCE
 
                     //START KMP
                     val kmpTime = measureTimeMillis {
-                        kmpOutputOccurrences = kmp(randomString, wzorzecInput)
+                        kmpOutput = kmp(randomString, wzorzecInput)
                     }
-                    kmpOutput.text = kmpTime.toString()
-                    kpmOccurrencesOutput.text = kmpOutputOccurrences.toString()
+                    kmpOutputTime.text = kmpTime.toString()
+                    //END KMP
+
+                    //START KMP
+                    val bmTime = measureTimeMillis {
+                        bmOutput = bm(randomString, wzorzecInput)
+                    }
+                    bmOutputTime.text = bmTime.toString()
+                    //END KMP
+
+                    //START RK
+                    val rkTime = measureTimeMillis {
+                        rkOutput = search(randomString, wzorzecInput, 101)
+                    }
+                    rkOutputTime.text = rkTime.toString()
+                    //END RK
+
+                    occurrencesOutput.text = "Occurrences: $bfOutput"
+                    firstPositionOutput.text = "First position: $kmpOutput"
+                    firstPositionOutput.text = "First position: $bmOutput"
+                    firstPositionOutput.text = "First position: $rkOutput"
 
                 }
                 else
